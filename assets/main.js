f7e24bd9658a53bd80919d95ce0b74313faa1c6d
Jennifer <git@rycz.com>
2017-08-21 14:18:55 -0500

    Coded main.js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
   // your code will go here
+  $.ajax('https://www.codeschool.com/users/rycz.json', {
+  	dataType: 'jsonp',
+  	success: function(response) {
+  		var badgeDiv = $('#badges');
+  		$.each(response.courses.completed, function(index, course) {
+  			var html = '<div class="course">';
+  			html += '<h3>' + course.title + '</h3>';
+  			html += '<img src="' + course.badge + '">';
+  			html += '<a class="btn btn-primary" target="_blank" href="' + course.url + '">See Course</a>';
+  			html += '</div>';
+  			badgeDiv.append(html);
+  		});
+  	}
+  });
 
 });
